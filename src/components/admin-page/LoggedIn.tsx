a548b7017275c3a39ac72db0541ffbab2b48c307
Franco May <franco.may@etraveligroup.com>
2023-02-28 22:17:48 +0100

    some styling to quill

--- src/components/admin-page/LoggedIn.tsx
+++ src/components/admin-page/LoggedIn.tsx
@@ -1,13 +1,14 @@
 import Layout from "../Layout";
 import dynamic from "next/dynamic";
 import { useAuth } from "@/providers/AuthProvider";
-// import RichTextArea from "./RichTextArea";
 const RichTextArea = dynamic(() => import("./RichTextArea"), { ssr: false });
 
 function LoggedIn() {
   const { logOut } = useAuth();
   return (
-    <Layout>
+    <Layout
+      contentStyles={{ alignItems: "stretch", justifyContent: "flex-start" }}
+    >
       <p>logged in</p>
       <button onClick={() => logOut()}>sign out</button>
       <RichTextArea />
