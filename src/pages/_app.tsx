44bb8642cd6a5c4b6ea22703c0ec6832517e2e28
Franco May <franco.may@etraveligroup.com>
2023-06-14 21:47:55 +0200

    added loading bar to the router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,12 @@ import { Analytics } from "@vercel/analytics/react";
 import Theme from "@/providers/theme/Theme";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { Toast } from "@/components/design-system";
+import NProgress from "nprogress";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
