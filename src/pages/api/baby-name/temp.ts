f619a32434ee73d2947cde572fc1be2795c105db
Franco May <francomay3@gmail.com>
2024-10-29 14:06:03 +0100

    open cors for baby name app

--- src/pages/api/baby-name/temp.ts
+++ src/pages/api/baby-name/temp.ts
@@ -1,11 +1,41 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 import admin from "@/config/firebase-admin";
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ["GET", "POST"], // Add the HTTP methods you need
+  origin: "*", // Be more restrictive in production
+  credentials: true,
+});
+
+// Helper method to wait for middleware
+const runMiddleware = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    callback: (result: any) => void
+  ) => void
+) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
+    await runMiddleware(req, res, cors);
+
     const token = req.headers.authorization?.split("Bearer ")[1];
 
     if (!token) {
